f8f37da4079c3e4a8126339a4049f3d1a8fb1aa9
noah the goodra <peterpan0413@live.com>
2017-08-01 22:08:04 -0500

    added code to load assetbundle

--- TestPlugin/Hackobject.cs
+++ TestPlugin/Hackobject.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using FallFlatHelpers;
+using UnityEngine;
 using UnityEngine.SceneManagement;
 
 partial class tests
@@ -15,16 +16,42 @@ partial class tests
         {
             if (Input.GetKeyDown(KeyCode.F3))
             {
-                //GenericHelpers.CreateGameObjectAndAttachClassAndAllowDestory<Bricktest>();
-                for (int sceneIndex = 0; sceneIndex < SceneManager.sceneCount; sceneIndex++)
-                {
-                    var sceneobject = SceneManager.GetSceneAt(sceneIndex);
-                    var scenename = sceneobject.name;
-                    var scenepath = sceneobject.path;
-                    var scenerootobjects = sceneobject.GetRootGameObjects();
+                Loadme();
+                //DumpSceneData();
+            }
+        }
 
-                    Debug.Log(scenename +":" +scenepath + ":" + scenerootobjects);
-                }
+        private static void Loadme()
+        {
+            var noahmodel = AssetBundle.LoadFromFile(Application.dataPath + "/" + "noah_model");
+            var asset = Instantiate(noahmodel.LoadAsset<GameObject>("Model_Noah_prefab"));
+            var playerposition = PlayerHelpers.GetPlayerHeadPosition();
+            var playerinstance = PlayerHelpers.GetPlayerMonoBehaviour() as Player;
+            asset.transform.position = new Vector3(playerposition.x, playerposition.y, playerposition.z);
+            //asset.AddComponent<SphereCollider>();
+
+            foreach (var bone in asset.GetComponents<GameObject>())
+            {
+                Debug.Log(bone);
+                bone.AddComponent<Rigidbody>();
+                bone.AddComponent<SphereCollider>();
+            }
+            //asset.transform.SetParent(playerinstance.gameObject.transform,false);
+
+            noahmodel.Unload(false);
+        }
+
+        private static void DumpSceneData()
+        {
+            //GenericHelpers.CreateGameObjectAndAttachClassAndAllowDestory<Bricktest>();
+            for (int sceneIndex = 0; sceneIndex < SceneManager.sceneCount; sceneIndex++)
+            {
+                var sceneobject = SceneManager.GetSceneAt(sceneIndex);
+                var scenename = sceneobject.name;
+                var scenepath = sceneobject.path;
+                var scenerootobjects = sceneobject.GetRootGameObjects();
+
+                Debug.Log(scenename + ":" + scenepath + ":" + scenerootobjects);
             }
         }
 
